3e0f827a30d7d84ef6e1d7cc809a1d5ffecbf7fa
Lucas Wagner <lowagner@gmail.com>
2022-06-21 20:58:59 -0700

    improved drawing algorithm
    TODO: improved collision detection

--- core/display.c
+++ core/display.c
@@ -20,6 +20,21 @@ static inline uint8_t *display() {
     return playdate->graphics->getFrame();
 }
 
+// pass in a pre-computed (pre-inverted) color here.
+// i.e., real_color = display_maybe_invert_color(color).
+static inline void display_byte_draw_with_mask(
+    uint8_t *buffer_at_byte,
+    uint8_t mask,
+    uint8_t real_color
+) {
+    *buffer_at_byte = (*buffer_at_byte & ~mask) | (real_color & mask);
+}
+
+static inline uint8_t display_maybe_invert_color(uint8_t color) {
+    // TODO: only bit-flip if not color inverted
+    return ~color;
+}
+
 void display_clear(uint8_t bg_color, display_slice slice) {
     if (slice.start_row >= slice.end_row || slice.start_row >= LCD_ROWS) {
         return;
@@ -28,7 +43,7 @@ void display_clear(uint8_t bg_color, display_slice slice) {
     uint8_t *display_buffer = display();
     memset(
         display_buffer + ROW_STRIDE * slice.start_row,
-        ~bg_color,
+        display_maybe_invert_color(bg_color),
         ROW_STRIDE * (slice.end_row - slice.start_row)
     );
     playdate->graphics->markUpdatedRows(slice.start_row, slice.end_row - 1);
@@ -38,12 +53,14 @@ void display_clear_alternating(uint8_t bg_color0, uint8_t bg_color1, display_sli
     if (slice.start_row >= slice.end_row || slice.start_row >= LCD_ROWS) {
         return;
     }
+    bg_color0 = display_maybe_invert_color(bg_color0);
+    bg_color1 = display_maybe_invert_color(bg_color1);
     // TODO: check for end_row past LCD_ROWS
     uint8_t *display_row = display() + ROW_STRIDE * slice.start_row;
     for (int row = slice.start_row; row < slice.end_row; ++row) {
         memset(
             display_row,
-            ~( row % 2 ? bg_color1 : bg_color0 ),
+            row % 2 ? bg_color1 : bg_color0,
             // note that ROW_STRIDE = 52 to do 32 bit alignments,
             // but only LCD_COLUMNS / 8 = 50 bytes are visible per row:
             LCD_COLUMNS / 8
@@ -83,25 +100,47 @@ void display_box_draw(uint8_t color, display_box box) {
     int last_bits = box.end_x % 8;
 
     uint8_t *const display_buffer = display();
+    color = display_maybe_invert_color(color);
 
-    if (start_bits) {
-        uint8_t first_bits_color = U8_BITMASK_RIGHT_DISPLAY_BITS(start_bits) & color;
-        for (int16_t row = box.start_y; row < box.end_y; ++row) {
-            display_buffer[ROW_STRIDE * row + first_full_byte - 1] &= ~first_bits_color;
+    if (last_full_byte - first_full_byte > 0) {
+        if (start_bits) {
+            uint8_t mask = U8_BITMASK_RIGHT_DISPLAY_BITS(start_bits);
+            for (int16_t row = box.start_y; row < box.end_y; ++row) {
+                display_byte_draw_with_mask(
+                    display_buffer + ROW_STRIDE * row + first_full_byte - 1,
+                    mask,
+                    color
+                );
+            }
         }
-    }
-    if (last_full_byte - first_full_byte > 0)
-    for (int16_t row = box.start_y; row < box.end_y; ++row) {
-        memset(
-            display_buffer + ROW_STRIDE * row + first_full_byte,
-            ~color,
-            last_full_byte - first_full_byte
-        );
-    }
-    if (last_bits) {
-        uint8_t last_bits_color = U8_BITMASK_LEFT_DISPLAY_BITS(last_bits) & color;
         for (int16_t row = box.start_y; row < box.end_y; ++row) {
-            display_buffer[ROW_STRIDE * row + last_full_byte] &= ~last_bits_color;
+            memset(
+                display_buffer + ROW_STRIDE * row + first_full_byte,
+                color,
+                last_full_byte - first_full_byte
+            );
+        }
+        if (last_bits) {
+            uint8_t mask = U8_BITMASK_LEFT_DISPLAY_BITS(last_bits);
+            for (int16_t row = box.start_y; row < box.end_y; ++row) {
+                display_byte_draw_with_mask(
+                    display_buffer + ROW_STRIDE * row + last_full_byte,
+                    mask,
+                    color
+                );
+            }
+        }
+    } else {
+        uint8_t mask = (
+                U8_BITMASK_RIGHT_DISPLAY_BITS(start_bits)
+            &   U8_BITMASK_LEFT_DISPLAY_BITS(last_bits)
+        );
+        for (int16_t row = box.start_y; row < box.end_y; ++row) {
+            display_byte_draw_with_mask(
+                display_buffer + ROW_STRIDE * row + last_full_byte,
+                mask,
+                color
+            );
         }
     }
     playdate->graphics->markUpdatedRows(box.start_y, box.end_y - 1);
