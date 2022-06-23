211698a78df3c24451e7becd9ab3debd360ac587
Lucas Wagner <lowagner@gmail.com>
2022-06-22 22:05:05 -0700

    switch to using dizzysnake directory for snake dev

---
+++ dizzysnake/src/main.c
@@ -0,0 +1,25 @@
+#include "modes.h"
+#include "../../core/core.h"
+
+DATA4(
+    const char *, value, "dizzysnake",
+    int, x, LCD_COLUMNS / 2,
+    int, y, LCD_ROWS / 2,
+    int, last_jump, 0,
+    title
+);
+
+void initialize() {
+    // TODO: better way of adding these tests programatically
+    #ifndef NDEBUG
+    test__core__display();
+    test__core__error();
+    test__mode__snake();
+    #endif
+    font_load("/System/Fonts/Asheville-Sans-14-Bold.pft");
+    runtime.transition.next_mode = kRuntimeModeSnake;
+}
+
+void default_update(display_slice slice) {
+    display_clear(17, slice);
+}
