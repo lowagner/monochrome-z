a297c2b984363bdc1b8b6030f2561a9c96a939b6
Lucas Wagner <lowagner@gmail.com>
2022-06-28 20:33:56 -0700

    add boolean data iterator with setters/getters

--- core/data.c
+++ core/data.c
@@ -1,1 +1,111 @@
 #include "data.h"
+
+#ifndef NDEBUG
+#include "error.h"
+#include <string.h> // memset
+#endif
+
+void data_booleans_initialize(data_booleans *booleans, uint32_t offset) {
+    booleans->byte_offset = offset / 8;
+    booleans->bit_offset = 7 - offset % 8; // we iterate over most-significant-bits first for playdate
+}
+
+void data_booleans_increment(data_booleans *booleans) {
+    if (--booleans->bit_offset > 7) {
+        // bit_offset went from 0 to 255
+        booleans->bit_offset = 7;
+        ++booleans->byte_offset;
+    }
+}
+
+void data_booleans_set(const data_booleans *booleans, uint8_t *data, int value) {
+    if (value) {
+        data[booleans->byte_offset] |= 1 << booleans->bit_offset;
+    } else {
+        data[booleans->byte_offset] &= ~(1 << booleans->bit_offset);
+    }
+}
+
+int data_booleans_get(const data_booleans *booleans, const uint8_t *data) {
+    return (data[booleans->byte_offset] >> booleans->bit_offset) & 1;
+}
+
+int data_booleans_flip(const data_booleans *booleans, uint8_t *data) {
+    data[booleans->byte_offset] ^= 1 << booleans->bit_offset;
+}
+#ifndef NDEBUG
+void test__core__data() {
+    data_booleans test_booleans;
+    uint8_t test_data[256];
+
+    TEST(
+        data_booleans_initialize(&test_booleans, 0);
+        EXPECT_INT_EQUAL_LOGGED(test_booleans.byte_offset, 0);
+        EXPECT_INT_EQUAL_LOGGED(test_booleans.bit_offset, 7);
+
+        data_booleans_initialize(&test_booleans, 23);
+        EXPECT_INT_EQUAL_LOGGED(test_booleans.byte_offset, 2);
+        EXPECT_INT_EQUAL_LOGGED(test_booleans.bit_offset, 0);
+
+        data_booleans_initialize(&test_booleans, 58);
+        EXPECT_INT_EQUAL_LOGGED(test_booleans.byte_offset, 7);
+        EXPECT_INT_EQUAL_LOGGED(test_booleans.bit_offset, 5),
+        "%s: booleans initializes correct with offsets",
+        AT
+    );
+
+    TEST(
+        data_booleans_initialize(&test_booleans, 0);
+        for (int i = 0; i < 256 * 8; ++i) {
+            data_booleans_set(&test_booleans, test_data, 0);
+            data_booleans_increment(&test_booleans);
+        }
+        for (int i = 0; i < 256; ++i) {
+            TEST(
+                EXPECT_INT_EQUAL_LOGGED(test_data[i], 0),
+                "%s: at byte offset %d",
+                AT, i
+            );
+        },
+        "%s: booleans can set a bunch of values to 0",
+        AT
+    );
+
+    TEST(
+        data_booleans_initialize(&test_booleans, 0);
+        for (int i = 0; i < 256 * 8; ++i) {
+            data_booleans_set(&test_booleans, test_data, 1);
+            data_booleans_increment(&test_booleans);
+        }
+        for (int i = 0; i < 256; ++i) {
+            TEST(
+                EXPECT_INT_EQUAL_LOGGED(test_data[i], 255),
+                "%s: at byte offset %d",
+                AT, i
+            );
+        },
+        "%s: booleans can set a bunch of values to 1",
+        AT
+    );
+
+    TEST(
+        memset(test_data, 127, 256);
+        data_booleans_initialize(&test_booleans, 0);
+        for (int i = 0; i < 256 * 8; ++i) {
+            if (i % 8 == 3) {
+                data_booleans_flip(&test_booleans, test_data);
+            }
+            data_booleans_increment(&test_booleans);
+        }
+        for (int i = 0; i < 256; ++i) {
+            TEST(
+                EXPECT_INT_EQUAL_LOGGED(test_data[i], 111),
+                "%s: at byte offset %d",
+                AT, i
+            );
+        },
+        "%s: booleans can flip bits",
+        AT
+    );
+}
+#endif
--- core/data.h
+++ core/data.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <stdint.h>
+
 #define DATA3(type1, name1, init1, type2, name2, init2, type3, name3, init3, struct_name) \
     struct struct_name { \
         type1 name1; \
@@ -25,3 +27,20 @@
         .name3 = init3, \
         .name4 = init4, \
     }
+
+typedef struct data_booleans {
+    uint32_t byte_offset;
+    // should be between 0 and 7.
+    uint8_t bit_offset;
+}
+    data_booleans;
+
+void data_booleans_initialize(data_booleans *booleans, uint32_t offset);
+void data_booleans_increment(data_booleans *booleans);
+// sets the data at the current booleans offset to 0 or 1, depending on
+// whether the value is 0 or non-zero.
+void data_booleans_set(const data_booleans *booleans, uint8_t *data, int value);
+// returns a 0 or a 1, depending on whether the bit is not set or set.
+int data_booleans_get(const data_booleans *booleans, const uint8_t *data);
+// flips the bit specified by booleans in data, i.e., from 0 to 1, or from 1 to 0.
+int data_booleans_flip(const data_booleans *booleans, uint8_t *data);
